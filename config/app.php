6075d2c9d434869f5947c0c9178ecd54eecde46d
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-08-02 19:13:07 +0700

    Update dependencies

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Laravel\Tinker\TinkerServiceProvider::class,
         UxWeb\SweetAlert\SweetAlertServiceProvider::class,
 
         //
