bca0d874297212233aea385350dfc2383a668ec0
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-08-02 19:08:24 +0700

    Update logout to POST

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,7 +66,18 @@
             </a>
 
             <ul class="dropdown-menu" role="menu">
-              <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+              <li>
+                <a href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"
+                >
+                  Logout
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+              </li>
             </ul>
           </li>
         @endif
